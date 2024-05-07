b324dc1db0bc815766ec665b5f3901c898a94202
nvanschaack <nellvanschaack@gmail.com>
2024-05-07 16:59:55 -0500

    completed fucntionality for index.js

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ function prompts() {
                     addEmployee()
                     break;
                 case 'Update an employee role':
+                    updateEmployeeRole()
                     break;
                 default:
                     process.exit();
@@ -82,7 +83,8 @@ function viewRoles() {
 
 function viewEmployees() {
     // WHEN I choose to view all employees THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-    pool.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name, role.salary, employee.manager_id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id', function (error, { rows }) {
+    pool.query("SELECT employee.id, CONCAT(employee.first_name, ' ', employee.last_name) AS employeeName, role.title, department.department_name, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS managerName FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee AS manager ON  manager.id = employee.manager_id;", function (error, { rows }) {
+
         console.table(rows);
         prompts()
     })
@@ -90,38 +92,65 @@ function viewEmployees() {
 
 function addDepartment() {
     // WHEN I choose to add a department THEN I am prompted to enter the name of the department and that department is added to the database
-    pool.query('SELECT * FROM department', function (error, { rows }) {
-        const departmentArray = rows.map((department) => ({
-            name: department.title,
-            value: department.id
-        }))
-        inquirer
+
+    inquirer
         .prompt([
             {
                 type: 'input',
                 name: 'department_name',
-                message: 'What is the name of the department you would like to add?',
-                choices: departmentArray
+                message: 'What is the name of the department you would like to add?'
             }
         ]).then((answers) => {
-            console.log(answers);
             //add the new department to the database
             pool.query(`INSERT INTO department (department_name) VALUES ($1)`, [answers.department_name], (error, { rows }) => {
                 if (error) {
                     console.error(error)
                 } else {
                     //returning an empty array
-                    console.log(rows)
+                    console.log('data inserted')
                     prompts()
                 }
             })
         })
-
-    })
 };
 
 function addRole() {
     // WHEN I choose to add a role THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+    pool.query('SELECT * FROM department', function (error, { rows }) {
+        const departmentArray = rows.map((department) => ({
+            name: department.department_name,
+            value: department.id
+        }))
+
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What would you like the title of the new role to be?',
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'What would you like the salary of the new role to be?',
+            },
+            {
+                type: 'list',
+                name: 'department_id',
+                message: 'What department would you like the new role to be in?',
+                choices: departmentArray
+            }
+        ]).then((response) => {
+            pool.query('INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)', [response.title, response.salary, response.department_id], (error, data) => {
+                if (error) {
+                    console.log(error)
+                }
+                console.log('Role has been added')
+                prompts()
+            })
+        })
+
+    })
 };
 
 function addEmployee() {
@@ -142,6 +171,11 @@ function addEmployee() {
                 }))
 
             // what if the new employee doesnt have a manager? how do you account for that? - add something in manager array that gives back a not applicable option
+
+            managerArray.push({
+                name: 'N/A',
+                value: null
+            })
 
             inquirer
                 .prompt([
@@ -170,12 +204,12 @@ function addEmployee() {
                 ]).then((answers) => {
                     console.log(answers);
                     //add the new employee to the database
-                    pool.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1)`, [lhhhliljlkj], (error, { rows }) => {
+                    pool.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)`, [answers.first_name, answers.last_name, answers.role_id, answers.manager_id], (error, { rows }) => {
                         if (error) {
                             console.error(error)
                         } else {
                             //returning an empty array
-                            console.log(rows)
+                            console.log('employee added')
                             prompts()
                         }
                     })
@@ -187,7 +221,43 @@ function addEmployee() {
 function updateEmployeeRole() {
     // WHEN I choose to update an employee role
     // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+    pool.query('SELECT * FROM employee', function (error, { rows }) {
+        const employeeArray = rows
+            .map((employee) => ({
+                name: `${employee.first_name} ${employee.last_name}`,
+                value: employee.id
+            }))
+        pool.query('SELECT * FROM role', function (error, { rows }) {
+            const roleArray = rows.map((role) => ({
+                name: role.title,
+                value: role.id
+            }))
 
-}
+            inquirer
+                .prompt([
+                    {
+                        type: 'list',
+                        name: 'employee_id',
+                        message: 'Which employee would you like to update?',
+                        choices: employeeArray
+                    },
+                    {
+                        type: 'list',
+                        name: 'role_id',
+                        message: 'Which role would you like to assign to the employee?',
+                        choices: roleArray
+                    }
+                ]).then((response) => {
+                    pool.query('UPDATE employee SET role_id = $1 WHERE id = $2', [response.role_id, response.employee_id], (error, data) => {
+                        if (error) {
+                            console.error(error)
+                        }
+                        console.log('employee updated')
+                        prompts()
+                    })
+                })
+        })
+    })
+};
 
 start()
